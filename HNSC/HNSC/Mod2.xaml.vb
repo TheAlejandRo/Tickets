858948cf63c3a613287da3a8fdcd14954d34ea2c
TheAlejandRo <alejandrocaal44@hotmail.com>
2018-10-25 08:58:19 -0600

    v1.0

--- HNSC/HNSC/Mod2.xaml.vb
+++ HNSC/HNSC/Mod2.xaml.vb
@@ -2,5 +2,9 @@
 Imports System.Data.SqlClient
 
 Public Class Mod2
+    Private Sub btn_sig_client_Click(sender As Object, e As RoutedEventArgs) Handles btn_sig_client.Click
+        'If My.Windows.m_Base.list_cod.Items = My.Settings.No_Ticket Then
 
+        'End If
+    End Sub
 End Class
